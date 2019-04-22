2138ea84c11112cf62e7277347b4ceb563aa8974
Mauricio_Gonzales <mauri_17spd@hotmail.com>
2019-04-22 08:36:52 -0400

    Commit primera revision exercise

--- src/main/java/org/umssdiplo/automationv01/core/utils/LoadPage.java
+++ src/main/java/org/umssdiplo/automationv01/core/utils/LoadPage.java
@@ -1,12 +1,12 @@
 package org.umssdiplo.automationv01.core.utils;
 
 import org.umssdiplo.automationv01.core.customwebdriver.ManageDriver;
-import org.umssdiplo.automationv01.core.managepage.Login.Login;
+import org.umssdiplo.automationv01.core.managepage.Adidas.Adidas;
 
 public final class LoadPage {
-    public static Login loginPage() {
+    public static Adidas adidasPage() {
         ManageDriver.getInstance().getWebDriver()
                 .navigate().to(PropertyAccessor.getInstance().getBaseUrl());
-        return new Login();
+        return new Adidas();
     }
 }
